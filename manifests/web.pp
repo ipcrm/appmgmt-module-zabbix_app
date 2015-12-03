8fd6cffe308df24f8f356ac806097987d8c90ef0
ipcrm <mcadorette@puppetlabs.com>
2015-12-03 15:53:23 -0500

    Fixed params for zabbix::web

--- manifests/web.pp
+++ manifests/web.pp
@@ -10,13 +10,13 @@ define zabbix_app::web (
   }
   class { 'apache::mod::php': }
   class { 'zabbix::web':
-    zabbix_url    => $::fqdn,
-    zabbix_server => $zabbix_server,
-    database_type => 'mysql',
-    database_host => $zabbix_db_host,
-    database_name => $zabbix_db_name,
-    database_user => $zabbix_db_user,
-    database_pass => $zabbix_db_pass,
+    zabbix_url        => $::fqdn,
+    zabbix_server     => $zabbix_server,
+    database_type     => 'mysql',
+    database_host     => $zabbix_db_host,
+    database_name     => $zabbix_db_name,
+    database_user     => $zabbix_db_user,
+    database_password => $zabbix_db_pass,
   }
 
 }
