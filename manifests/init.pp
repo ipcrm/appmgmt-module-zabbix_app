f2071a33607eb66eaead72623c91347862f9000b
ipcrm <mcadorette@puppetlabs.com>
2015-12-03 14:16:44 -0500

    Updated Application Definition
    The calls to defines within
    the application declaration where not using titles.....

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@ application zabbix_app(
   $database_user,
   $database_password,
 ){
-  zabbix_app::db {
+  zabbix_app::db{ $name:
     zabbix_server     => $zabbix_server,
     zabbix_web        => $zabbix_web,
     database_name     => $database_name,
@@ -14,11 +14,11 @@ application zabbix_app(
     export            => ZabbixDb["zbx-${name}"]
   }
 
-  zabbix_app::server {
+  zabbix_app::server { $name:
     consume => ZabbixDb["zbx-${name}"]
   }
 
-  zabbix_app::web {
+  zabbix_app::web { $name:
     zabbix_server => $zabbix_server,
     consume       => ZabbixDb["zbx-${name}"]
   }
